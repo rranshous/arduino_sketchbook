a3926bb5ea5419fdc3a585dbcc1b574d1b3bd1a5
Robby Ranshous <rranshous@gmail.com>
2016-04-04 21:28:25 -0400

    who needs safety?

--- bin/matrix_gui.rb
+++ bin/matrix_gui.rb
@@ -4,8 +4,7 @@ require 'thread'
 WRITER_PATH=ENV['WRITE_SERIAL']
 
 def write_serial text
-  @serial_mutex ||= Mutex.new
-  @serial_mutex.synchronize { system("#{WRITER_PATH} \"#{text}\"") }
+  system("#{WRITER_PATH} \"#{text}\"")
 end
 
 def set_note note
@@ -13,19 +12,23 @@ def set_note note
 end
 
 def write_console text
-  @write_mutex ||= Mutex.new
   @note.replace text
 end
 
 def clear_on_delay
-  Thread.new { sleep(5) and write_serial('') }
+  Thread.new {
+    sleep(2)
+    write_serial('')
+    write_console('-')
+  }
 end
 
 Shoes.app {
-  set_note para "Status Here", size: 'x-large'
+  set_note para "init", size: 'x-large'
   flow {
     image("smile.png").click do
       write_console "smiling"
+      puts "SMILING"
       write_serial("smile") and clear_on_delay or write_console("failed to smile")
     end
     image("frown.png").click do
