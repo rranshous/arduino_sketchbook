1171ec637f701cf29f67d77a673f2bd676429724
Robby Ranshous <rranshous@gmail.com>
2016-04-04 21:20:34 -0400

    clear after 5 seconds

--- bin/matrix_gui.rb
+++ bin/matrix_gui.rb
@@ -1,20 +1,36 @@
+#!/usr/bin/env shoes
+require 'thread'
 
 WRITER_PATH=ENV['WRITE_SERIAL']
 
 def write_serial text
-  system("#{WRITER_PATH} \"#{text}\"")
+  @serial_mutex ||= Mutex.new
+  @serial_mutex.synchronize { system("#{WRITER_PATH} \"#{text}\"") }
+end
+
+def set_note note
+  @note = note
+end
+
+def write_console text
+  @write_mutex ||= Mutex.new
+  @note.replace text
+end
+
+def clear_on_delay
+  Thread.new { sleep(5) and write_serial('') }
 end
 
 Shoes.app {
-  @note = para "Status Here", size: 'x-large'
+  set_note para "Status Here", size: 'x-large'
   flow {
     image("smile.png").click do
-      @note.replace "smiling"
-      write_serial("smile") or @note.replace "failed to smile"
+      write_console "smiling"
+      write_serial("smile") and clear_on_delay or write_console("failed to smile")
     end
     image("frown.png").click do
-      @note.replace "frowning"
-      write_serial("frown") or @note.replace "failed to frown"
+      write_console "frowning"
+      write_serial("frown") and clear_on_delay or write_console("failed to frown")
     end
   }
   #stack { }
